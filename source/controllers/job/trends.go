89dec2830718c0bc41f63c5f75574412f8570c3c
Sean Cheng <ptp_sean@photons.com.tw>
2023-10-19 22:13:49 +0800

    WIP

--- source/controllers/job/trends.go
+++ source/controllers/job/trends.go
@@ -1,6 +1,7 @@
 package job
 
 import (
+	"github.com/SeanZhenggg/go-utils/logger"
 	"github.com/gin-gonic/gin"
 	"lizard/source/service"
 )
@@ -9,16 +10,21 @@ type ITrendCtrl interface {
 	FetchTrends(ctx *gin.Context)
 }
 
-func ProviderITrendsJobCtrl(trendSrv service.ITrendSrv) ITrendCtrl {
+func ProviderITrendsJobCtrl(trendSrv service.ITrendSrv, logger logger.ILogger) ITrendCtrl {
 	return &trendJobCtrl{
 		trendSrv: trendSrv,
+		logger:   logger,
 	}
 }
 
 type trendJobCtrl struct {
 	trendSrv service.ITrendSrv
+	logger   logger.ILogger
 }
 
 func (t *trendJobCtrl) FetchTrends(ctx *gin.Context) {
-
+	err := t.trendSrv.FetchTrends(ctx)
+	if err != nil {
+		return
+	}
 }
