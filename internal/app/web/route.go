b82446a952ad38cf31543aefb504276845f4753d
Sean Cheng <ptp_sean@photons.com.tw>
2023-11-03 02:06:29 +0800

    update route for line webhook

--- internal/app/web/route.go
+++ internal/app/web/route.go
@@ -7,11 +7,11 @@ import (
 
 func (app *webApp) setApiRoutes(g *gin.Engine) {
 	g.GET("health", func(ctx *gin.Context) { ctx.JSON(http.StatusOK, gin.H{"message": "ok"}) })
+	g.POST("/line/webhook", app.Ctrl.MessageCtrl.RecvMessage)
 
 	webGroup := g.Group("")
 	webGroup.Use(app.RespMw.Handle)
 	webGroup.GET("/r/:path", app.Ctrl.TrendCtrl.RedirectToTrendPage)
-	webGroup.POST("/line/webhook", app.Ctrl.MessageCtrl.RecvMessage)
 
 	apiGroup := g.Group("/api")
 	apiGroup.Use(app.RespMw.Handle)
