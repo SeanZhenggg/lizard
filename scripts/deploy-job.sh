f78078c51e03a76df2dd327456d9808fc248150e
Sean Cheng <ptp_sean@photons.com.tw>
2023-11-05 00:15:06 +0800

    update deploy shell script

--- scripts/deploy-job.sh
+++ scripts/deploy-job.sh
@@ -5,4 +5,4 @@ eval `ssh-agent -s`
 ssh-add /root/.ssh/sean_github_id_ed25519
 go env -w GOPRIVATE=github.com/SeanZhenggg
 go get ./...
-go build -o ./main ./cmd/job && ./main
+go build -buildvcs=false -o ./main ./cmd/job && ./main
