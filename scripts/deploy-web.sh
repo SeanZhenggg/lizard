a7ece6e8a591f16685a95ee642e1c7bbacb18081
Sean Cheng <ptp_sean@photons.com.tw>
2023-11-04 23:13:31 +0800

    update deploy related sources

---
+++ scripts/deploy-web.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+apk update && apk upgrade && apk add git openssh
+eval `ssh-agent -s`
+ssh-add /root/.ssh/sean_github_id_ed25519
+go env -w GOPRIVATE=github.com/SeanZhenggg
+go get ./...
+go build -o ./main ./cmd/web && ./main
