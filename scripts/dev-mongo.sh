5895c75823911450f4c6604110146fb2ef16e687
Sean Cheng <ptp_sean@photons.com.tw>
2023-11-11 09:59:54 +0800

    update script

--- scripts/dev-mongo.sh
+++ scripts/dev-mongo.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-docker run -dit --rm -p 27017:27017 -e MONGO_INITDB_ROOT_USERNAME="root" -e MONGO_INITDB_ROOT_PASSWORD="1qaz@WSX" -v ./build/mongodb/init.js:/docker-entrypoint-initdb.d/init.js --name lizard-db lizard-db mongod
+docker run -dit --rm -p 27017:27017 -e MONGO_INITDB_ROOT_USERNAME="root" -e MONGO_INITDB_ROOT_PASSWORD="mongopw" -v ./build/mongodb/init.js:/docker-entrypoint-initdb.d/init.js --name lizard-db lizard-db mongod
